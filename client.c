c2f307657d09912c9e5e23ad6284a19d4f48871e
Denis Semenov <dsemenov@student.42lyon.fr>
2025-02-15 19:42:32 +0100

    Add handshake. Not malloc yet and no sending of length

--- client.c
+++ client.c
@@ -5,66 +5,89 @@
 #include "./libft/ft_printf.h"
 #include "./libft/libft.h"
 
-extern int g_pid;
+volatile sig_atomic_t g_ack_received = 0;
 
-void  send_msg(char c, pid_t server_pid)
+void  ack_handler(int signo)
+{
+  (void) signo;
+  g_ack_received = 1;
+}
+
+void  send_bit(unsigned char bit, pid_t server_pid)
+{
+  g_ack_received = 0;
+  if (bit)
+    kill(server_pid, SIGUSR1);
+  else
+    kill(server_pid, SIGUSR2);
+  
+  while (!g_ack_received)
+    pause();
+}
+
+void  send_char(char c, pid_t server_pid)
 {
   int  i;
   unsigned char bit;
-
- 
-    unsigned char byte = c;
-    i = 7;
-    while (i >= 0)
-    {
-      bit = byte >> i & 1;
-      if (bit == 1)
-        kill(server_pid, SIGUSR1);
-      else
-        kill(server_pid, SIGUSR2);
-      usleep (5000);
-      i--;
-    }
-}
-
-void send_strlen(size_t len, pid_t server_pid)
-{
-  int i;
-  unsigned char bit;
-
-  i = 31;
+  unsigned char byte;
+    
+  byte = c;
+  i = 7;
   while (i >= 0)
   {
-    bit = len >> i & 1;
-    if (bit == 1)
-      kill(server_pid, SIGUSR1);
-    else
-      kill(server_pid, SIGUSR2);
-    usleep(5000);
+    bit = byte >> i & 1;
+    send_bit(bit, server_pid);
     i--;
   }
 }
+
+// void send_strlen(size_t len, pid_t server_pid)
+// {
+//   int i;
+//   unsigned char bit;
+
+//   i = 31;
+//   while (i >= 0)
+//   {
+//     bit = len >> i & 1;
+//     if (bit == 1 && g_ack_received == 0)
+//       kill(server_pid, SIGUSR1);
+//     else if (bit == 0 && g_ack_received == 0)
+//       kill(server_pid, SIGUSR2);
+//     else
+//       exit(1);
+//     usleep(1000);
+//     g_ack_received = 1;
+//     i--;
+//   }
+// }
+
 
 int main(int argc, char **argv)
 {
   pid_t server_pid;
   char *msg;
-  size_t len;
+  //size_t len;
   size_t i;
+  struct sigaction  sa;
 
   if (argc != 3)
   {
     ft_printf("Wrong number of arguments!\n");
     return (1);
   }
+  sigemptyset(&sa.sa_mask);
+  sa.sa_handler = ack_handler;
+  sa.sa_flags = 0;
+  sigaction(SIGUSR1, &sa, NULL);
   server_pid = ft_atoi(argv[1]);
   msg = argv[2];
-  len = ft_strlen(msg);
-  send_strlen(len, server_pid);
+  //len = ft_strlen(msg);
+  // send_strlen(len, server_pid);
   i = 0;
   while (msg[i])
   {
-    send_msg(msg[i], server_pid);
+    send_char(msg[i], server_pid);
     i++;
   }
   return (0);
--- server.c
+++ server.c
@@ -5,89 +5,52 @@
 #include "./libft/ft_printf.h"
 #include "./libft/libft.h"
  
-// int g_pid;
-
-// void  handler(int signo, siginfo_t *info, void *context)
-// {
-// 	(void)info;
-// 	(void)context;
-// 	static unsigned char byte;
-// 	static int bit_count;
-
-//   if (signo == SIGUSR1)
-// 	  byte = (byte << 1) | 1;
-//   else if (signo == SIGUSR2)
-//     byte <<= 1;
-
-// 	bit_count++;
-
-// 	if (bit_count == 8)
-// 	{
-// 		ft_putchar(byte);
-// 		byte = 0;
-// 		bit_count = 0;
-//     return ;
-// 	}
-// }
-
-// void  len_handler(int signo, siginfo_t *info, void *context)
-// {
-// 	(void)info;
-// 	(void)context;
-// 	static unsigned int i;
-// 	static int bit_count;
-
-//   if (signo == SIGUSR1)
-// 	  i = (i << 1) | 1;
-//   else if (signo == SIGUSR2)
-//     i <<= 1;
-
-// 	bit_count++;
-
-// 	if (bit_count == 32)
-// 	{
-// 		ft_putnbr(i);
-// 		i = 0;
-// 		bit_count = 0;
-//     return;
-// 	}
-// }
-
-volatile sig_atomic_t g_receiving_length = 1;  // 1 - получаем длину, 0 - получаем сообщение
-volatile unsigned int g_length = 0;
-volatile int g_len_bit_count = 0;
-volatile unsigned char g_char = 0;
-volatile int g_char_bit_count = 0;
-
-void universal_handler(int signo, siginfo_t *info, void *context)
+void  handler(int signo, siginfo_t *info, void *context)
 {
-    (void)info;
-    (void)context;
-    
-    if (g_receiving_length)
-    {
-        // Принимаем длину (32 бита)
-        g_length = (g_length << 1) | (signo == SIGUSR1 ? 1 : 0);
-        g_len_bit_count++;
-        if (g_len_bit_count == 32)
-        {
-            ft_printf("Length: %u\n", g_length);
-            g_receiving_length = 0;  // переключаемся на приём сообщения
-        }
-    }
-    else
-    {
-        // Принимаем символ (8 бит)
-        g_char = (g_char << 1) | (signo == SIGUSR1 ? 1 : 0);
-        g_char_bit_count++;
-        if (g_char_bit_count == 8)
-        {
-            ft_putchar(g_char);
-            g_char = 0;
-            g_char_bit_count = 0;
-        }
-    }
+	(void)context;
+	static unsigned char byte = 0;
+	static int bit_count = 0;
+
+    if (signo == SIGUSR1)
+        byte = (byte << 1) | 1;
+    else if (signo == SIGUSR2)
+        byte <<= 1;
+
+bit_count++;
+
+if (bit_count == 8)
+{
+    ft_putchar(byte);
+    byte = 0;
+    bit_count = 0;
 }
+kill(info->si_pid, SIGUSR1);
+}
+
+void  len_handler(int signo, siginfo_t *info, void *context)
+{
+	(void)info;
+	(void)context;
+	static unsigned int i;
+	static int bit_count;
+
+  if (signo == SIGUSR1)
+	  i = (i << 1) | 1;
+  else if (signo == SIGUSR2)
+    i <<= 1;
+
+kill(info->si_pid, SIGUSR1);
+	bit_count++;
+
+	if (bit_count == 32)
+	{
+		ft_putnbr(i);
+		i = 0;
+		bit_count = 0;
+    return;
+	}
+}
+
 
 int main(void)
 {
@@ -96,7 +59,7 @@ int main(void)
 
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = universal_handler;
+    sa.sa_sigaction = handler;
     sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGUSR2, &sa, NULL);
 
