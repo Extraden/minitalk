354154ac8a5da64e46a07d4480e87d8ad6daddbe
Denis Semenov <dsemenov@student.42lyon.fr>
2025-02-26 15:52:36 +0100

    Last fixes

--- client.c
+++ client.c
@@ -1,88 +1,80 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <signal.h>
-#include <sys/types.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 16:34:42 by dsemenov          #+#    #+#             */
+/*   Updated: 2025/02/24 18:16:34 by dsemenov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./libft/ft_printf.h"
 #include "./libft/libft.h"
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-volatile sig_atomic_t g_ack_received = 0;
+volatile sig_atomic_t	g_ack_received = 0;
 
-void  ack_handler(int signo)
+void	ack_handler(int signo)
 {
-  (void) signo;
-  g_ack_received = 1;
+	if (signo == SIGUSR2)
+		ft_printf("The message is sent!");
+	g_ack_received = 1;
 }
 
-void  send_bit(unsigned char bit, pid_t server_pid)
+void	send_bit(unsigned char bit, pid_t server_pid)
 {
-  g_ack_received = 0;
-  if (bit)
-    kill(server_pid, SIGUSR1);
-  else
-    kill(server_pid, SIGUSR2);
-  
-  while (!g_ack_received)
-    pause();
+	g_ack_received = 0;
+	if (bit)
+		kill(server_pid, SIGUSR1);
+	else
+		kill(server_pid, SIGUSR2);
+	while (!g_ack_received)
+		pause();
 }
 
-void  send_char(char c, pid_t server_pid)
+void	send_char(char c, pid_t server_pid)
 {
-  int  i;
-  unsigned char bit;
-  unsigned char byte;
-    
-  byte = c;
-  i = 7;
-  while (i >= 0)
-  {
-    bit = byte >> i & 1;
-    send_bit(bit, server_pid);
-    i--;
-  }
+	int				i;
+	unsigned char	bit;
+	unsigned char	byte;
+
+	byte = c;
+	i = 7;
+	while (i >= 0)
+	{
+		bit = byte >> i & 1;
+		send_bit(bit, server_pid);
+		i--;
+	}
 }
 
-void send_strlen(size_t len, pid_t server_pid)
+int	main(int argc, char **argv)
 {
-  int i;
-  unsigned char bit;
+	pid_t				server_pid;
+	char				*msg;
+	size_t				i;
+	struct sigaction	sa;
 
-  i = 31;
-  while (i >= 0)
-  {
-    bit = len >> i & 1;
-    send_bit(bit, server_pid);   
-    i--;
-  }
-}
-
-int main(int argc, char **argv)
-{
-  pid_t server_pid;
-  char *msg;
-  size_t len;
-  size_t i;
-  struct sigaction  sa;
-
-  if (argc != 3)
-  {
-    ft_printf("Wrong number of arguments!\n");
-    return (1);
-  }
-  sigemptyset(&sa.sa_mask);
-  sa.sa_handler = ack_handler;
-  sa.sa_flags = 0;
-  sigaction(SIGUSR1, &sa, NULL);
-  server_pid = ft_atoi(argv[1]);
-  msg = argv[2];
-  len = ft_strlen(msg);
-  send_strlen(len, server_pid);
-  i = 0;
-  while (msg[i])
-  {
-    send_char(msg[i], server_pid);
-    i++;
-  }
-  send_char('\n', server_pid);
-
-  return (0);
+	if (argc != 3)
+	{
+		ft_printf("Wrong number of arguments!\n");
+		return (1);
+	}
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = ack_handler;
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	server_pid = ft_atoi(argv[1]);
+	msg = argv[2];
+	i = -1;
+	while (msg[++i])
+		send_char(msg[i], server_pid);
+	send_char('\n', server_pid);
+	send_char('\0', server_pid);
+	return (0);
 }
--- server.c
+++ server.c
@@ -1,73 +1,56 @@
-#include <sys/types.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsemenov <dsemenov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/24 16:34:49 by dsemenov          #+#    #+#             */
+/*   Updated: 2025/02/24 18:06:55 by dsemenov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./libft/ft_printf.h"
 #include "./libft/libft.h"
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int	g_len_received = 0;
- 
-void  handler(int signo, siginfo_t *info, void *context)
+void	handler(int signo, siginfo_t *info, void *context)
 {
+	static unsigned char	byte = 0;
+	static int				bit_count = 0;
+
 	(void)context;
-	static unsigned char byte = 0;
-	static int bit_count = 0;
-
-    if (signo == SIGUSR1)
-        byte = (byte << 1) | 1;
-    else if (signo == SIGUSR2)
-        byte <<= 1;
-
-bit_count++;
-
-if (bit_count == 8)
-{
-    ft_putchar(byte);
-    byte = 0;
-    bit_count = 0;
-}
-kill(info->si_pid, SIGUSR1);
-}
-
-void  len_handler(int signo, siginfo_t *info, void *context)
-{
-	(void)info;
-	(void)context;
-	static unsigned int i;
-	static int bit_count;
-
-  if (signo == SIGUSR1)
-	  i = (i << 1) | 1;
-  else if (signo == SIGUSR2)
-    i <<= 1;
-
-kill(info->si_pid, SIGUSR1);
+	if (signo == SIGUSR1)
+		byte = (byte << 1) | 1;
+	else if (signo == SIGUSR2)
+		byte <<= 1;
 	bit_count++;
-
-	if (bit_count == 32)
+	if (bit_count == 8)
 	{
-		ft_putnbr(i);
-		i = 0;
+		if (byte == 0)
+			kill(info->si_pid, SIGUSR2);
+		ft_putchar(byte);
+		byte = 0;
 		bit_count = 0;
-    return;
 	}
+	kill(info->si_pid, SIGUSR1);
 }
 
-
-int main(void)
+int	main(void)
 {
-    struct sigaction sa;
-    pid_t pid;
+	struct sigaction	sa;
+	pid_t				pid;
 
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = handler;
-    sigaction(SIGUSR1, &sa, NULL);
-    sigaction(SIGUSR2, &sa, NULL);
-
-    pid = getpid();
-    ft_printf("%d\n", pid);
-    while (1)
-        pause();
-    return (0);
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	pid = getpid();
+	ft_printf("%d\n", pid);
+	while (1)
+		pause();
+	return (0);
 }
